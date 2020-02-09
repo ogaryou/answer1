5b2911b59ed24dbfc29f9866397a21f0e7e08684
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-02-09 18:53:59 +0900

    コントローラー作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,23 @@
 class QuestionsController < ApplicationController
+  def index
+    @question = Question.all
+    @response = Response.all
+  end  
+
   def new
     @question = Question.new
   end  
+
+  def create
+    Question.create(question_params)
+    Response.create(response_params)
+  end  
+
+  private
+  def question_params
+    params.require(:question).permit(:body)
+  end
+  
+  def response_params
+    params.require(:response).permit(:body)
 end  
