366ad22d0e37010286fb5a0715a11a5846d5c951
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-02-29 23:16:16 +0900

    一問一答機能

--- app/assets/javascripts/qa.js
+++ app/assets/javascripts/qa.js
@@ -15,25 +15,41 @@
 
 
 $(function(){
+  $('.main__head__questions').html(gon.sample[gon.count].body);
   $('.main__bootom__botun__click__function').on('click', function(){
-    console.log('今晩わ');
-    document.getElementById("question1").textContent= "解答";
 
-    $('#main__bootom__botun__click2').slideToggle();　
-    $('#questions2').toggle();
-    const p1 = document.getElementById("answer1");
-    const p2 = document.getElementById("next_qa");
-    if(p1.style.display=="block"){
-        // noneで非表示
-        p1.style.display ="none";
-    }else{
-        // blockで表示
-        p1.style.display ="block";
-    }
+    gon.count += 0;
+    var num = gon.count + 1;
+
+    $('.main__head__question').html('解答' + num + '/' + gon.sample.length);
+
+    $('.main__head__questions').html(gon.sample[gon.count].content);
+    if (num === gon.sample.length) {
+      $('.main__bootom__botun__click__function__next').hide();
+      $('.main__bootom__botun__click__function').hide();
+      $('.main__head__question').html('お疲れ様でした');
+    };
   })
   $('.main__bootom__botun__click__function__next').on('click', function(){
     console.log('嫌だ');
     // document.getElementById("question1").textContent= "問題";
-    location.href = "./index";
+    gon.count += 1;
+    var num = gon.count + 1;
+    $.ajax({
+      url: '/qas/index',
+      type: 'GET',
+      dataType: 'HTML',
+      async: true,
+      data: {
+        count: gon.count,
+        sample: gon.sample,
+      },
+    });
+
+    $('.main__head__question').html('問題' + num + '/' + gon.sample.length);
+    $('.main__head__questions').html(gon.sample[gon.count].body);
+
   })
 })
+
+
