f60a4faf1f6037e4ec7045721e25bd66ae397154
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-03-06 00:13:26 +0900

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  devise_for :users
+  root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'answers#index'
+  resources :qas
   get 'qas/index', to: 'qas#index'
   post '/qas/index',to: 'qas#index'
-  resources :qas
   delete 'qas/destroy', to: 'qas#destroy'
   post '/qas/new',to: 'qas#create'
   
