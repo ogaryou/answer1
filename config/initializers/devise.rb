01d8cb85b655c58e092c8c6e70bb5cddfd0db823
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-03-07 19:01:33 +0900

    コントローラーの作成

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :twitter, "YOUR_CONSUMER_KEY", "YOUR_CONSUMER_SECRET", "https://localhost:8000/users/auth/twitter/callback"
+  config.omniauth :twitter, "YOUR_CONSUMER_KEY", "YOUR_CONSUMER_SECRET"
 end
