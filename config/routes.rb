29a712c23e86edbfe929184111a5db0d2b9c85f6
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-03-09 20:04:59 +0900

    viewの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :qas
   get 'qas/index', to: 'qas#index'
   post '/qas/index',to: 'qas#index'
   delete 'qas/destroy', to: 'qas#destroy'
   post '/qas/new',to: 'qas#create'
-  get '/answers/index',to:'answers#index'
+  root 'answers#index'
+  get 'answers/index'
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   
 end
