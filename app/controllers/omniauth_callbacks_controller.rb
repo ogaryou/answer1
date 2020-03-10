1d134beede26985ce20aef4547a39a4769eebad8
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-03-11 07:35:08 +0900

    リダイレクトの設定

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  
   def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
     if @user.persisted?
@@ -9,5 +10,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
+  get 'mypage', to: 'users#me'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :qas
+  resources :users, only: %i[new create]
   get 'qas/index', to: 'qas#index'
   post 'qas/index', to: 'qas#index'
   patch "/qas" , to:'qas#index'
   delete 'qas/destroy', to: 'qas#destroy'
   post '/qas/new',to: 'qas#create'
   root 'answers#index'
-  get 'answers/index'
+  get 'answers/index',to: 'answers#index', as: :user_root
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
 end
