4c1c0e4ed4a88afe9e6f896a3a6c4e5c8bdec01f
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-03-11 09:19:36 +0900

    current_user設定

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  before_action :current_user
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+  end  
 end
